5297ade110123c1b311437272850ad0b60060d1e
Etelcastro <etelacastro22@gmail.com>
2019-06-03 09:38:24 -0500

    agrega proyecto parcial

---
+++ cipher.js
@@ -0,0 +1,8 @@
+function go(){
+    if (document.form.password.value=='LABORATORIA' && document.form.login.value=='USUARIO'){ 
+            document.form.submit(); 
+        } 
+        else{ 
+             alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+        } 
+    }
